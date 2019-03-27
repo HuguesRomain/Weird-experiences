8c86b934ac5a3db7ae653e7541129d45aff15eb6
HuguesRomain <huguesromainpro@gmail.com>
2019-03-27 16:56:40 +0100

    different chart ype

--- graphic with reactjs and chartjs/src/App.js
+++ graphic with reactjs and chartjs/src/App.js
@@ -4,15 +4,55 @@ import './App.css';
 import CanvasChart from './components/Chart';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData: {},
+    };
+  }
+
+  componentWillMount(){
+    this.getChartdata(); 
+  }
+
+getChartdata(){
+  //Ajax calls here
+  this.setState({
+    chartData: {
+      labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+      datasets:[
+        {
+          label:'Population',
+          data:[
+            615794,
+            181045,
+            153060,
+            106519,
+            105162,
+            95072
+          ],
+          backgroundColor:[
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)',
+            'rgba(255, 99, 132, 0.6)'
+          ]
+        }
+      ]
+    }
+  })
+}
   render() {
-    console.log(Date())
     return (
       <div className="App">
         <div className="App-header" style={{height: 2}}>
           <img src={logo} className='App-logo' alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <CanvasChart legendPosition='bottom' />
+        <CanvasChart chartData={this.state.chartData} location='Massachusetts' legendPosition='bottom' />
       </div>
     );
   }
--- graphic with reactjs and chartjs/src/components/Chart.js
+++ graphic with reactjs and chartjs/src/components/Chart.js
@@ -4,39 +4,16 @@ import {Bar, Line, Pie} from 'react-chartjs-2'
 class CanvasChart extends Component {
   constructor(props){
     super(props);
-    this.state = ({
-      chartData:{
-        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
-        datasets:[
-          {
-            label:'Population',
-            data:[
-              615794,
-              181045,
-              153060,
-              106519,
-              105162,
-              95072
-            ],
-            backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
-            ]
-          }
-        ]
-      }
-    });
+    this.state = {
+      chartData: props.chartData,
+    };
   }
 
   static defaultProps = {
     displayTitle:true,
     displayLegend:true,
     legendPosition:'right',
+    location: 'City',
   }
 
   render () {
@@ -47,7 +24,37 @@ class CanvasChart extends Component {
         options={{ 
           title:{
            display: this.props.displayTitle,
-           text: 'Largest cities of Massachusetts',
+           text: `Largest Cities in ${this.props.location}`,
+           fontSize: 24,
+          },
+          legend:{
+            display: this.props.displayLegend,
+            position: this.props.legendPosition
+          }
+         }}
+        />
+
+        <Line
+        data={this.state.chartData}
+        options={{ 
+          title:{
+           display: this.props.displayTitle,
+           text: `Largest Cities in ${this.props.location}`,
+           fontSize: 24,
+          },
+          legend:{
+            display: this.props.displayLegend,
+            position: this.props.legendPosition
+          }
+         }}
+        />
+
+        <Pie
+        data={this.state.chartData}
+        options={{ 
+          title:{
+           display: this.props.displayTitle,
+           text: `Largest Cities in ${this.props.location}`,
            fontSize: 24,
           },
           legend:{
