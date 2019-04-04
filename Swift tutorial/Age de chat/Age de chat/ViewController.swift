f068c7576302e06dc7d8c094cd539599e9f4cbc1
HuguesRomain <huguesromainpro@gmail.com>
2019-04-04 22:25:17 +0200

    swift app  init

---
+++ Swift tutorial/Age de chat/Age de chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Age de chat
+//
+//  Created by Hugues ROMAIN  on 04/04/2019.
+//  Copyright © 2019 Hugues ROMAIN . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
