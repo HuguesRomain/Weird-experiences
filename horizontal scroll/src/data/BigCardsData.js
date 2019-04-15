2b0715649b4d992ddc3694480d2355978642649a
HuguesRomain <huguesromainpro@gmail.com>
2019-04-15 23:51:54 +0200

    data city savavve

---
+++ horizontal scroll/src/data/BigCardsData.js
@@ -0,0 +1,30 @@
+const CityCardsData = {
+CityCards: {
+  elements1: {
+    title: 'New York',
+    img: 'NewYork.png'
+    },
+    elements2: {
+      title: 'New York',
+      img: 'NewYork.png'
+    },
+    elements3: {
+      title: 'New York',
+      img: 'NewYork.png'
+    },
+    elements4: {
+      title: 'New York',
+      img: 'NewYork.png'
+    },
+    elements5: {
+      title: 'New York',
+      img: 'NewYork.png'
+    },
+    elements6: {
+      title: 'New York',
+      img: 'NewYork.png'
+    },
+  },
+};
+
+export default LogBookData;
