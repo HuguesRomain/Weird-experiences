4763c7158b9443ed90533e5b5ac447a6f36ff80e
HuguesRomain <huguesromainpro@gmail.com>
2019-05-09 22:35:26 +0200

    pseudo in url router

--- base-chatbox-app/src/components/Connexion.js
+++ base-chatbox-app/src/components/Connexion.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import {Redirect} from 'react-router-dom'
 
 class Connexion extends Component {
+  state = {
+    pseudo: '',
+    goToChat: false, 
+  }
+
+  handleChange = event => {
+    const pseudo = event.target.value
+    this.setState({ pseudo })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault() 
+    this.setState({ goToChat: true})
+  }
+
   render () {
+    if(this.state.goToChat) {
+      return <Redirect to={`/pseudo/${this.state.pseudo}`}/>
+    }
+
     return (
       <div className="connexionBox">
-        <form className="connexion">
+        <form className="connexion" onSubmit={this.handleSubmit}>
            <input type="text" 
+           value={this.state.pseudo}
+           onChange={this.handleChange}
            placeholder='pseudo'
            required/>
            <button type='submit'>GO !</button>
