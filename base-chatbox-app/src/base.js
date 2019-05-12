59e6c7fcff0585e058eb148e99a1f9ab0e0cdccd
HuguesRomain <huguesromainpro@gmail.com>
2019-05-12 17:14:00 +0200

    firebase init

---
+++ base-chatbox-app/src/base.js
@@ -0,0 +1,16 @@
+import {Rebase} from 're-base'; 
+import firebase from 'firebase/app';
+import 'firebase/database'; 
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyB0UfLbVlYKU-SW4Smasv-4lpTY37p6TOI",
+    authDomain: "chatbox-react-ee753.firebaseapp.com",
+    databaseURL: "https://chatbox-react-ee753.firebaseio.com",
+})
+
+const base = Rebase.createClass(firebase.database())
+
+export {firebaseApp}
+
+
+export default {base}
