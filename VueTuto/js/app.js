e603e56a6a87ccc4dd4954641bf74fed61d0a114
HuguesRomain <huguesromainpro@gmail.com>
2019-05-20 22:05:00 +0200

    vue tuto

---
+++ VueTuto/js/app.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data: {
+      message: "salut les gens",
+      link: "https://cdnjs.com/libraries/vue", 
+      success: true,
+      names: ["Michel", "Jean", "Diarit", "Maksym", "Louis"], 
+  },
+})
