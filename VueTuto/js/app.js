b91d5d9011ae1fb2490ab32783771caa19148bf4
HuguesRomain <huguesromainpro@gmail.com>
2019-05-21 10:53:49 +0200

    instance vue

--- VueTuto/js/app.js
+++ VueTuto/js/app.js
@@ -1,9 +1,32 @@
-new Vue({
-  el: '#app',
+// new Vue({
+//   el: '#app',
+//   data: {
+//       message: "salut les gens",
+//       link: "https://cdnjs.com/libraries/vue", 
+//       success: true,
+//       names: ["Michel", "Jean", "Diarit", "Maksym", "Louis"], 
+//   },
+//   methods: {
+//     addPerson: function () {
+//       this.names.push = ('Marion')
+//     },
+//   },
+//   beforeCreate() {
+//     console.log(this.$el)
+//   },
+// }); 
+
+let vm = new Vue({
+  el: "#App", 
   data: {
-      message: "salut les gens",
-      link: "https://cdnjs.com/libraries/vue", 
-      success: true,
-      names: ["Michel", "Jean", "Diarit", "Maksym", "Louis"], 
+    seconds: 0 
+  }, 
+  mounted() {
+    this.$interval = setInterval(() => {
+      this.seconds++; 
+    }, 1000); 
   },
+  destroyed() {
+    clearInterval(this.$tnterval)
+  }
 })
