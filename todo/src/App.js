6c81f433ffced6de3094b84108ba24ca2e8a35e7
HuguesRomain <huguesromainpro@gmail.com>
2019-06-05 16:04:42 +0200

    I will be back

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './assets/reset.css'
 import logo from './logo.svg';
 import './App.css'; 
+import styled, {ThemeProvider} from 'styled-components';
 import Header from './components/Header/Header';
 import Navigation from './components/Header/Navigation'
 import Plus from '/Users/hugues/Documents/Documents pro/Git Project/Weird-experiences/todo/src/components/AddingTodo/plus.js'
@@ -11,15 +12,40 @@ class App extends Component {
   state = {
     AddTodo: false,
   }
+  handlePlus = () => {
+    if(this.state.AddTodo === false){
+    this.setState({AddTodo: true})
+    } else {
+      this.setState({AddTodo: false})
+    }
+  }
+
   render(){
     return (
       <div className="App">
       <Header></Header>
       <Navigation />
-      <Plus />
+      <BlackBackground AddTodo={this.state.AddTodo}/>
+      <Plus onClick={this.handlePlus}/>
       </div>
     );
   }
 }
+
+
+const BlackBackground = styled.div`
+  display: flex;
+  position: fixed;
+  height: 100vh; 
+  width: 100vw;
+  background-color: black; 
+  opacity: 0.70;
+  top: 0;
+`
+
+
+
+
+
 export default App;
 
